48e3d938ce0d7a99c60c3f2a845b60ac15efd8c7
choiseonggyung <sg0975@naver.com>
2025-06-13 14:27:04 +0900

    Add files via upload

---
+++ firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbHpo-G0zEo0Y6IrnGvT_ALAFAuz4mCPo",
+  authDomain: "oneheart-guardians.firebaseapp.com",
+  projectId: "oneheart-guardians",
+  storageBucket: "oneheart-guardians.firebasestorage.app",
+  messagingSenderId: "661686568819",
+  appId: "1:661686568819:web:cfb6ebbac4056b0a956706"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
